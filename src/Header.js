6b1845fa423adbaf480804b85a0ad2880443dd51
AKRVerz <kurniawansyahapri@gmail.com>
2021-11-20 10:10:09 +0700

    Update Right Navbar

--- src/Header.js
+++ src/Header.js
@@ -2,24 +2,34 @@ import React from 'react'
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import logo from './logo.svg';
+import avatar from './avatar.jpg'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 
 function Header() {
     return (
-        <div className='header'>
-            <h1>Header Component</h1>
-
+        <div className='header'> 
             <div className="header__left">
                 <img src={logo} alt=""/>
 
-                <div className="header__serach">
+                <div className="header__search">
                     <SearchIcon />
                     <input type="text"/>
                 </div>
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar={avatar} title="Me" />
             </div>
         </div>
     )
